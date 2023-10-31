736023cdfa9ca7d6b9d0157b7ca5c43a8f9cc414
yasirozdemir <myozdemir.career@gmail.com>
2023-10-31 19:24:51 +0300

    #15

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,7 +3,7 @@ import { LinkPrimary } from "../Reusables/LinkPrimary";
 
 const Landing = () => {
   return (
-    <section id="landing" className="h-screen">
+    <section id="landing" className="h-dynamic-screen">
       <div className="flex flex-col justify-center items-center h-full z-10">
         <div className="text-3xl text-center md:text-4xl lg:text-5xl">
           Hey, I'm{" "}
