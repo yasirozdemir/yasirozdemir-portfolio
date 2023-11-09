fe395782e1f95a7a56eb50135a881d750e58736b
yasirozdemir <myozdemir.career@gmail.com>
2023-11-09 16:43:31 +0300

    #37

--- src/components/Reusables/ProjectCard.tsx
+++ src/components/Reusables/ProjectCard.tsx
@@ -12,7 +12,7 @@ export const ProjectCard = ({
     "text-delft_blue hover:text-oxford_blue font-bold inline-block border-b-[2px] transition-all border-delft_blue hover:border-oxford_blue leading-4";
   return (
     <article className="rounded-sm shadow-2xl grid grid-rows-1 md:grid-cols-2 sm:gap-4 md:gap-10 overflow-hidden bg-french_gray">
-      <div className="my-auto overflow-hidden rounded-sm">
+      <div className="my-auto overflow-hidden rounded-l-sm">
         <img src={img_src} alt={title.toLowerCase() + " web view"} />
       </div>
       <div className="flex flex-col justify-center items-center md:items-start py-4 lg:py-0 px-4 sm:pr-10">
