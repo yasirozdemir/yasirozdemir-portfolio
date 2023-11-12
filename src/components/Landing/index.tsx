9f9b7ddc23401ab4e4463841a64d3062f8feef0a
yasirozdemir <myozdemir.career@gmail.com>
2023-11-12 15:09:44 +0300

    #47

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -3,26 +3,27 @@ import { CV_URLs } from "../../data";
 
 const Landing = () => {
   const landingLinkClassNames =
-    "border-b-[3px] text-white border-white transition-all duration-300 hover:text-mikado_yellow hover:border-mikado_yellow";
+    "border-b-[2px] text-white border-white transition-all duration-300 hover:text-mikado_yellow hover:border-mikado_yellow";
 
   return (
     <section id="landing" className="h-dynamic-screen relative bg-oxford_blue">
-      <div className="absolute top-0 h-[50%] w-full bg-cover bg-center bg-oxford_blue bg-[url(https://res.cloudinary.com/yasirdev/image/upload/v1680299481/portfolio/bg_yfxulf.jpg)]" />
-      <div className="flex flex-col justify-center items-center h-full z-10 relative">
-        <div
-          className="mb-10 max-w-[15rem] sm:max-w-sm overflow-hidden shadow-2xl border-[8px] border-oxford_blue"
-          style={{ borderRadius: "70% 30% 70% 30% / 30% 73% 27% 70%" }}
-        >
-          <img
-            src="/assets/yo.jpeg"
-            alt="Yasir Ozdemir Photo"
-            className="max-w-full h-auto align-middle italic bg-no-repeat bg-cover bg-[url(/assets/yo-lowres.jpeg)]"
-            width="500px"
-            height="500px"
-          />
+      <div className="h-full grid grid-rows-2">
+        <div className="bg-[url(https://res.cloudinary.com/yasirdev/image/upload/v1680299481/portfolio/bg_yfxulf.jpg)] flex justify-center items-end bg-no-repeat bg-cover bg-center">
+          <div
+            className="max-w-[15rem] sm:max-w-sm overflow-hidden shadow-2xl border-[8px] border-oxford_blue translate-y-1/4 sm:translate-y-1/3"
+            style={{ borderRadius: "70% 30% 70% 30% / 30% 73% 27% 70%" }}
+          >
+            <img
+              src="/assets/yo.jpeg"
+              alt="Yasir Ozdemir Photo"
+              className="max-w-full h-auto align-middle italic bg-no-repeat bg-cover bg-[url(/assets/yo-lowres.jpeg)]"
+              width="500px"
+              height="500px"
+            />
+          </div>
         </div>
-        <div className="text-center">
-          <h1 className="text-5xl md:text-6xl font-semibold text-mikado_yellow">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-semibold text-mikado_yellow">
             Yasir Ozdemir
           </h1>
           <TypewriterComponent
@@ -30,9 +31,10 @@ const Landing = () => {
               autoStart: true,
               loop: true,
               cursor: "|",
-              cursorClassName: "Typewriter__cursor text-2xl md:text-3xl",
+              cursorClassName:
+                "Typewriter__cursor text-xl sm:text-2xl md:text-3xl",
               wrapperClassName:
-                "Typewriter__wrapper text-2xl md:text-3xl font-secondary",
+                "Typewriter__wrapper text-xl sm:text-2xl md:text-3xl font-secondary",
             }}
             onInit={(typewriter) => {
               typewriter
@@ -49,7 +51,7 @@ const Landing = () => {
             }}
           />
         </div>
-        <div className="mt-3 sm:mt-4 md:mt-5 lg:mt-10 flex flex-col md:flex-row gap-2 md:gap-6 text-lg md:text-xl font-extrabold items-center justify-center">
+        <div className="mt-auto mb-5 flex flex-col md:flex-row gap-2 md:gap-6 sm:text-lg md:text-xl font-bold items-center justify-center">
           <a href="#about" className={landingLinkClassNames}>
             Get to know me
           </a>
