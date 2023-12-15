3bcf8e454c1a96769246855e27980818d4e47154
yasirozdemir <myozdemir.career@gmail.com>
2023-12-15 13:31:20 +0300

    #51

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,9 +8,9 @@ import { H3 } from "../Reusables/H3";
 
 const About = () => {
   const badgeClassList =
-    "px-4 py-2 rounded-md text-[#CFCFCF] shadow-[0_0_15px_0_rgba(0,0,0,0.5)_inset] bg-oxford_blue font-semibold";
+    "badge px-4 py-2 rounded-md text-[#CFCFCF] shadow-[0_0_15px_0_rgba(0,0,0,0.5)_inset] bg-oxford_blue font-semibold";
 
-  const pClassList = "text-md md:text-lg";
+  const pClassList = "text-md md:text-lg mb-4 [&>b]:text-mikado_yellow";
 
   return (
     <section id="about" className="py-10">
@@ -18,41 +18,25 @@ const About = () => {
         <div className="grid grid-rows-1 md:grid-cols-2 gap-10">
           <div>
             <H3 innerText="Get to know me!" customClasses="text-white" />
-            <p className={pClassList + " mb-4"}>
-              As a Full-Stack Developer, I enjoy creating{" "}
-              <span className="text-mikado_yellow">innovative</span> and
-              <span className="text-mikado_yellow"> impactful</span> solutions
-              using cutting-edge technologies. I have recently graduated from
-              EPICODE, a rigorous and intensive full-stack developer boot camp
-              that taught me how to use
-              <span className="text-mikado_yellow">
-                {" "}
-                TypeScript, React, Node.js, Express.js, and MongoDB
-              </span>{" "}
-              to build scalable and user-friendly web applications.
+            <p className={pClassList}>
+              As a Full-Stack Developer, I enjoy creating <b>innovative</b> and
+              <b> impactful</b> solutions using cutting-edge technologies. I
+              have recently graduated from EPICODE, a rigorous and intensive
+              full-stack developer boot camp that taught me how to use
+              <b> TypeScript, React, Node.js, Express.js, and MongoDB</b> to
+              build scalable and user-friendly web applications.
             </p>
-            <p className={pClassList + " mb-4"}>
-              I also have{" "}
-              <span className="text-mikado_yellow">
-                {" "}
-                diverse international experience
-              </span>
-              , having worked at Great Wolf Lodge in the United States, an
-              Erasmus+ project participation in France, and online digital
-              mentorship at Rome Business School. Through these opportunities, I
-              developed my
-              <span className="text-mikado_yellow">
-                {" "}
-                teamwork, communication, and intercultural skills
-              </span>{" "}
-              while learning from colleagues with different backgrounds and
-              perspectives.{" "}
-              <span className="text-mikado_yellow">
-                I am looking for a job
-              </span>{" "}
-              as a full-stack developer in a dynamic and innovative company
-              where I can keep enhancing my skills, contribute to exciting
-              projects, and make a difference. Check my{" "}
+            <p className={pClassList}>
+              I also have <b> diverse international experience</b>, having
+              worked at Great Wolf Lodge in the United States, an Erasmus+
+              project participation in France, and online digital mentorship at
+              Rome Business School. Through these opportunities, I developed my
+              <b> teamwork, communication, and intercultural skills</b> while
+              learning from colleagues with different backgrounds and
+              perspectives. <b>I am looking for a job</b> as a full-stack
+              developer in a dynamic and innovative company where I can keep
+              enhancing my skills, contribute to exciting projects, and make a
+              difference. Check my{" "}
               <a
                 href="https://www.linkedin.com/in/yasirozdemir"
                 target="_blank"
@@ -64,12 +48,10 @@ const About = () => {
             </p>
             <p className={pClassList}>
               In addition to my professional identity, I have a different list
-              of hobbies. The first one is{" "}
-              <span className="text-mikado_yellow">aviation</span>. I have
-              always been an aviation geek since I was a kid. I like to spend
-              time on flightradar24. Second,{" "}
-              <span className="text-mikado_yellow">I love to take photos</span>{" "}
-              with my phone while I travel around the globe. You can check my{" "}
+              of hobbies. The first one is <b>aviation</b>. I have always been
+              an aviation geek since I was a kid. I like to spend time on
+              flightradar24. Second, <b>I love to take photos</b> with my phone
+              while I travel around the globe. You can check my{" "}
               <a
                 href="https://www.instagram.com/yasirunofficial_"
                 target="_blank"
@@ -78,10 +60,10 @@ const About = () => {
                 Instagram
               </a>{" "}
               to see the photos taken by me. Adding to these two, I like{" "}
-              <span className="text-mikado_yellow">
+              <b>
                 riding a bike, hiking, working out at the gym, reading books,
                 watching series, and listening to music and podcasts.
-              </span>
+              </b>
             </p>
           </div>
           <div>
